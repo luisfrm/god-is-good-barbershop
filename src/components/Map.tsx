3936be287709f449c192c4f1722a874792dcbacf
Luis Rivas <luisfrm_@outlook.com>
2024-11-22 03:46:55 -0400

    created map section

---
+++ src/components/Map.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Map = () => {
+  return (
+    <section className='w-full'>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d490.13455004109403!2d-71.60899162211676!3d10.65117275483672!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1732258677222!5m2!1sen!2sus" width="100%" height="400" allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+    </section>
+  )
+}
+
+export default Map
