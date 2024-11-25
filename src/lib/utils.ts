ccd8fb890af4a06171e11f8678cf8dc10749f9b4
Luis Rivas <Luisfrm_@outlook.com>
2024-11-25 02:36:12 -0400

    added variables into utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const mapUrl = "https://maps.app.goo.gl/wMSAPhbnzKqrcQhY8"
+
+export const whatsappUrl = "https://api.whatsapp.com/send?phone=584246248690"
