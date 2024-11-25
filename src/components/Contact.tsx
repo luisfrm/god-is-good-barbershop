9abc37b1ee919709c7b81454145bfa40451beaa7
Luis Rivas <Luisfrm_@outlook.com>
2024-11-25 02:36:03 -0400

    added whatsapp and map url into variables on utils

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,8 @@
 import { Mail, MapPin, Phone } from 'lucide-react';
 import Link from 'next/link';
 import { Button } from './ui/button';
+import { mapUrl, whatsappUrl } from '@/lib/utils';
+import H3_Heading from './H3_Heading';
 
 
 interface Contact {
@@ -13,15 +15,15 @@ interface Contact {
 const contact: Contact[] = [
 	{
 		title: "Dirección",
-		description: "Calle 89, -bajando Av7-, Maracaibo",
+		description: "Calle 89 Av. 7, Maracaibo",
 		icon: <MapPin className="h-12 w-12 text-primary" />,
-    url: "https://maps.app.goo.gl/wMSAPhbnzKqrcQhY8"
+    url: mapUrl
 	},
   {
     title:"Teléfono",
     description: "+584246248690",
     icon: <Phone className="h-12 w-12 text-primary" />,
-    url: "https://api.whatsapp.com/send?phone=584246248690"
+    url: whatsappUrl
   },
   {
     title: "Email",
@@ -33,23 +35,42 @@ const contact: Contact[] = [
 
 const Contact = () => {
 	return (
-		<section id="contact" className="w-full py-28 bg-gray-100">
+		<section id="contact" className="w-full py-10 bg-gray-100">
 			<div className="max-w-6xl mx-auto text-center">
-				<h3 className="text-4xl mb-12 font-bold">Contacto</h3>
-				<div className="grid grid-cols-1 lg:grid-cols-3 justify-center gap-12 md:gap-8">
+				<H3_Heading>Contacto</H3_Heading>
+				<div className="grid grid-cols-1 lg:grid-cols-3 justify-center gap-8 mt-2">
 					{contact.map((item, index) => (
 						<div className="flex flex-col gap-0 md:gap-4 items-center" key={index}>
-							<span className='mb-3 md:mb-0'>{item.icon}</span>
-							<h4 className='font-semibold text-4xl font-serif'>{item.title}</h4>
-							{item.url ? <Link className='text-lg' target='_blank' href={item.url}>{item.description}</Link> : <p>{item.description}</p>}
+							<span className="mb-3 md:mb-0">{item.icon}</span>
+							<h4 className="font-semibold text-4xl font-serif">{item.title}</h4>
+							{item.url ? (
+								<Link className="text-lg" target="_blank" href={item.url}>
+									{item.description}
+								</Link>
+							) : (
+								<p>{item.description}</p>
+							)}
 						</div>
 					))}
 				</div>
-        <Link target='_blank' href="https://api.whatsapp.com/send?phone=584246248690">
-					<Button variant="default" className="shadow-sm border-0 mt-12">
-						Reserva tu cita
-					</Button>
-				</Link>
+				{(mapUrl || whatsappUrl) && (
+					<div className="grid grid-cols-1 px-4 md:flex gap-2 items-center justify-center mt-10">
+						{whatsappUrl && (
+							<Link target="_blank" href={whatsappUrl}>
+								<Button variant="default" className="w-full shadow-sm hover:opacity-90">
+									Reserva tu cita
+								</Button>
+							</Link>
+						)}
+						{mapUrl && (
+							<Link target="_blank" href={mapUrl}>
+								<Button variant="secondary" className="w-full shadow-sm border border-black bg-white hover:opacity-90">
+									Ver ubicación
+								</Button>
+							</Link>
+						)}
+					</div>
+				)}
 			</div>
 		</section>
 	);
